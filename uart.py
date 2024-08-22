83f3f59b364fdbc90e275cf71a64fadea7a9740f
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-08-22 13:13:57 +0300

    new script

--- uart.py
+++ uart.py
@@ -1,22 +1,29 @@
 import serial
 from time import sleep
-"""
-ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)  # timeout eklemek veri okuma süresini belirler
+
+# Seri portu açıyoruz ve baud rate olarak 9600 bps (bit per second) kullanıyoruz
+ser = serial.Serial("/dev/ttyS0", 9600)
 
 while True:
-    if ser.in_waiting > 0:  # Seri portta veri varsa
-        received_data = ser.read(ser.in_waiting)  # Tüm veriyi oku
-        print(received_data.decode('utf-8', errors='ignore'))  # Veriyi çöz ve yazdır
-        ser.write(received_data)  # Veriyi tekrar gönder
-    sleep(0.1)  # Döngü hızını ayarla
-"""
-
-
-ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)  # timeout eklemek veri okuma süresini belirler
-
-while True:
+    # Seri porttan 1 byte veri okuyoruz
     received_data = ser.read()
-    if ser.in_waiting > 0:  # Seri portta veri varsa
-        received_data = ser.read()  # Tüm veriyi oku
-        print(received_data.decode('utf-8', errors='ignore'))  # Veriyi çöz ve yazdır
-    sleep(0.001)  # Döngü hızını ayarla
+
+    # 30 milisaniye bekliyoruz. Bu, verinin gelmesini beklemek için kullanılır.
+    # Özellikle seri porttan gelen verilerin tam olarak alınabilmesi için bu süre zarfında
+    # veri akışını gözlemleyebiliriz. Bu, veri kaybını önlemek için önemlidir.
+    sleep(0.03)
+
+    # Seri portta hala okunacak veri olup olmadığını kontrol ediyoruz.
+    data_left = ser.inWaiting()
+
+    # Eğer veri varsa, kalan veriyi okuyoruz ve önceki okunan veriye ekliyoruz.
+    # `ser.inWaiting()` işlevi, portta bekleyen byte sayısını döner.
+    # `ser.read(data_left)` ile kalan byte'ları okuruz ve bu veriyi `received_data`'ya ekleriz.
+    received_data += ser.read(data_left)
+
+    # Alınan tüm veriyi ekrana yazdırıyoruz.
+    print(received_data)
+
+    # Alınan veriyi tekrar aynı seri port üzerinden gönderiyoruz.
+    # Bu işlem, verinin aynı şekilde geri gönderilmesini sağlar.
+    ser.write(received_data)
