4993910cf2a4f2571fb8c27b2fa13647489845a5
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-08-22 12:06:03 +0300

    uart

--- uart.py
+++ uart.py
@@ -4,7 +4,7 @@ http://www.electronicwings.com
 '''
 import serial
 from time import sleep
-
+received_data = ""
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
     #received_data = ser.read()              #read serial port
