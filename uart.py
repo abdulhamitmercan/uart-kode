1d454b125674592293cdfa8056b2bb709e709e1e
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-08-22 12:13:19 +0300

    meli

--- uart.py
+++ uart.py
@@ -1,15 +1,11 @@
-'''
-UART communication on Raspberry Pi using Pyhton
-http://www.electronicwings.com
-'''
 import serial
 from time import sleep
 
-ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)  # timeout eklemek veri okuma süresini belirler
+
 while True:
-    received_data = ser.read()              #read serial port
-    sleep(0.03)
-    data_left = ser.inWaiting()             #check for remaining byte
-    received_data += ser.read(data_left)
-    print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially 
+    if ser.in_waiting > 0:  # Seri portta veri varsa
+        received_data = ser.read(ser.in_waiting)  # Tüm veriyi oku
+        print(received_data.decode('utf-8', errors='ignore'))  # Veriyi çöz ve yazdır
+        ser.write(received_data)  # Veriyi tekrar gönder
+    sleep(0.1)  # Döngü hızını ayarla
