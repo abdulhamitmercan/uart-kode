dfe40d2001eef691f93f64d3193ea3406f86457a
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-08-22 12:10:31 +0300

    getde

--- uart.py
+++ uart.py
@@ -5,11 +5,11 @@ http://www.electronicwings.com
 import serial
 from time import sleep
 
-ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
-
+ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
-    if ser.in_waiting > 0:
-        received_data = ser.read(ser.in_waiting)  # Veriyi oku
-        print(received_data.decode('utf-8', errors='ignore'))  # Veriyi yazdır
-        ser.write(received_data)  # Veriyi tekrar gönder
-    sleep(0.1)  # Döngü hızını ayarla
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
