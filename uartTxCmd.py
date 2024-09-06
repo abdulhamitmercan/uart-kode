c11e224754d3e167d2ea4a685beb9abc348b3d9d
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-09-06 09:40:15 +0300

    asdfxfb

--- uartTxCmd.py
+++ uartTxCmd.py
@@ -1,18 +1,22 @@
-from UARTASYNC1 import UARTFrame, recieveframe, sendframe, RxTxFonk
-from uart_PROTOCOL import UartProtokol, RxTxFonk
 import asyncio
+from uart_PROTOCOL import UartProtokol
+from UARTASYNC1 import RxTxFonk
 
+async def main():
+    rxtx_fonk = RxTxFonk()
+    myUart = UartProtokol(rxtx_fonk)
+    
+    await asyncio.gather(
+        rxtx_fonk.send_message(),
+        rxtx_fonk.receive_message(),
+        myUart.handleUartFrame()
+    )
 
-
+asyncio.run(main())
 
 
 
 
 STOP_CHARGE = 2
 START_CHARGE = 1
-async def main():
-    rxtx_fonk = RxTxFonk()
-    myUart = UartProtokol(rxtx_fonk)    
-    await asyncio.gather(rxtx_fonk.send_message(), rxtx_fonk.receive_message(), myUart.handleUartFrame())
-    
-asyncio.run(main())
+
